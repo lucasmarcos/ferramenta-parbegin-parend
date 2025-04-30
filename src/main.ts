38214d5a5af3f10abb9ce786a0c0ae4b0020ac6a
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-04-30 20:01:11 -0300

    forkjoin copy

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { Parser, Language } from "web-tree-sitter";
+
+let parser: Parser;
+let ParBeginParEnd: Language;
+
+(async () => {
+  await Parser.init();
+  parser = new Parser();
+  let ParBeginParEnd = await Language.load("tree-sitter-parbeginparend");
+  parser.setLanguage(ParBeginParEnd);
+})();
