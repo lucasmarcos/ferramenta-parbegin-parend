d8dbd853636e7af3806517dae80ff09aef22496b
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-04-30 20:07:09 -0300

    editor view

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { Parser, Language } from "web-tree-sitter";
+import { basicSetup } from "codemirror";
+import { EditorView } from "@codemirror/view";
+
+const editorView: HTMLElement = document.getElementById("editor");
+const treeContainer: HTMLElement = document.getElementById("treeContainer");
+
+let editor;
 
 let parser: Parser;
 let ParBeginParEnd: Language;
@@ -6,6 +13,12 @@ let ParBeginParEnd: Language;
 (async () => {
   await Parser.init();
   parser = new Parser();
-  let ParBeginParEnd = await Language.load("tree-sitter-parbeginparend.wasm");
-  parser.setLanguage(ParBeginParEnd);
+  // let ParBeginParEnd = await Language.load("tree-sitter-parbeginparend.wasm");
+  // parser.setLanguage(ParBeginParEnd);
+  editor = new EditorView({
+    extensions: [
+      basicSetup,
+    ],
+    parent: editorView,
+  });
 })();
