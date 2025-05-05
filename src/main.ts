7a8de9de49197d19a4fb6e84ea37d9d177366498
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-05-05 17:12:14 -0300

    example

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,17 @@ import { EditorView } from "@codemirror/view";
 const editorView: HTMLElement = document.getElementById("editor");
 const treeContainer: HTMLElement = document.getElementById("treeContainer");
 
+const example = `BEGIN
+  A;
+  PARBEGIN
+    B;
+    C;
+    D;
+  PAREND
+  E;
+END
+`;
+
 let editor;
 
 let parser: Parser;
@@ -13,12 +24,16 @@ let ParBeginParEnd: Language;
 (async () => {
   await Parser.init();
   parser = new Parser();
-  // let ParBeginParEnd = await Language.load("tree-sitter-parbeginparend.wasm");
-  // parser.setLanguage(ParBeginParEnd);
+  let ParBeginParEnd = await Language.load("tree-sitter-parbeginparend.wasm");
+  parser.setLanguage(ParBeginParEnd);
   editor = new EditorView({
     extensions: [
       basicSetup,
     ],
     parent: editorView,
+    doc: example,
   });
+
+  const tree = parser.parse(example);
+  console.log(tree.rootNode.toString());
 })();
