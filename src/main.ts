626b0ef2c20fe9761a0ee290a2ff98edca28c010
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-05-05 17:44:48 -0300

    push/pop

--- src/main.ts
+++ src/main.ts
@@ -36,4 +36,41 @@ let ParBeginParEnd: Language;
 
   const tree = parser.parse(example);
   console.log(tree.rootNode.toString());
+
+  console.log(tree.rootNode);
+
+  let q = [];
+
+  for (let i = 0; i < tree.rootNode.childCount; i++) {
+    const type = tree.rootNode.child(i).type;
+
+    switch (type) {
+      case "begin": {
+        q.push("begin");
+        break;
+      }
+      case "end": {
+        const last = q.pop();
+        if (last !== "begin") {
+          console.log("error");
+        }
+        break;
+      }
+      case "parbegin": {
+        q.push("parbegin");
+        break;
+      }
+      case "parend": {
+        const last = q.pop();
+        if (last !== "parbegin") {
+          console.log("error");
+        }
+        break;
+      }
+      case "call": {
+        console.log(tree.rootNode.child(i).child(0).text);
+        break;
+      }
+     }
+  }
 })();
