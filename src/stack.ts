66672836ac0542c31e69cc787c9f9f66a5481599
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-05-06 16:12:45 -0300

    for of

--- src/stack.ts
+++ src/stack.ts
@@ -2,22 +2,22 @@ export const stackify = (ir) => {
 	const stack = [];
 	let current = [];
 
-	for (let i = 0; i < ir.length; i++) {
-		if (ir[i] === "[") {
+	for (const cmd of ir) {
+		if (cmd === "[") {
 			const node = { type: "seq", child: current };
 			stack.push(node);
 			current = [];
-		} else if (ir[i] === "(") {
+		} else if (cmd === "(") {
 			const node = { type: "par", child: current };
 			stack.push(node);
 			current = [];
-		} else if (ir[i] === "]" || ir[i] === ")") {
+		} else if (cmd === "]" || cmd === ")") {
 			const last = stack.pop();
 			const node = { type: last.type, child: current };
 			current = last.child;
 			current.push(node);
 		} else {
-			current.push({ type: "call", id: crypto.randomUUID(), label: ir[i] });
+			current.push({ type: "call", id: crypto.randomUUID(), label: cmd });
 		}
 	}
 
