5ad0cac03a48534155a4cf555b16d83e4f0e5513
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-05-06 15:16:21 -0300

    second ir

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-import { Parser, Language } from "web-tree-sitter";
-import { basicSetup } from "codemirror";
 import { EditorView } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+import cytoscape from "cytoscape";
+import dagre from "cytoscape-dagre";
+import { Language, Parser } from "web-tree-sitter";
 
 const editorView: HTMLElement = document.getElementById("editor");
 const treeContainer: HTMLElement = document.getElementById("treeContainer");
+
+cytoscape.use(dagre);
 
 const example = `BEGIN
   A;
@@ -26,119 +30,154 @@ let editor;
 let parser: Parser;
 let ParBeginParEnd: Language;
 
+const findFirstLabel = (node) => {
+	if (node.type === "call") {
+		return node.label;
+	} else if (node.type === "seq" || node.type === "par") {
+		return findFirstLabel(node.child[0]);
+	}
+	return null;
+};
+
+const interpret = (node, last, parentNext = null) => {
+	if (node.type === "seq") {
+		for (let i = 0; i < node.child.length; i++) {
+			const child = node.child[i];
+			const next = node.child[i + 1]
+				? findFirstLabel(node.child[i + 1])
+				: parentNext;
+
+			last = interpret(child, last, next);
+		}
+		return last;
+	} else if (node.type === "par") {
+		const outputs = [];
+
+		for (const child of node.child) {
+			const output = interpret(child, last, parentNext);
+			outputs.push(output);
+		}
+
+		for (const output of outputs) {
+			if (output && parentNext) {
+				console.log(`"${output}" -> "${parentNext}";`);
+			}
+		}
+
+		return parentNext;
+	} else if (node.type === "call") {
+		if (last) {
+			console.log(`"${last}" -> "${node.label}";`);
+		}
+		return node.label;
+	}
+};
+
 (async () => {
-  await Parser.init();
-  parser = new Parser();
-  let ParBeginParEnd = await Language.load("tree-sitter-parbeginparend.wasm");
-  parser.setLanguage(ParBeginParEnd);
-  editor = new EditorView({
-    extensions: [
-      basicSetup,
-    ],
-    parent: editorView,
-    doc: example,
-  });
+	await Parser.init();
+	parser = new Parser();
+	const ParBeginParEnd = await Language.load("tree-sitter-parbeginparend.wasm");
+	parser.setLanguage(ParBeginParEnd);
+	editor = new EditorView({
+		extensions: [basicSetup],
+		parent: editorView,
+		doc: example,
+	});
 
-  const tree = parser.parse(example);
+	const tree = parser.parse(example);
 
-  let q = [];
-  let ir = [];
+	const q = [];
+	const ir = [];
 
-  for (let i = 0; i < tree.rootNode.childCount; i++) {
-    const type = tree.rootNode.child(i).type;
+	for (let i = 0; i < tree.rootNode.childCount; i++) {
+		const type = tree.rootNode.child(i).type;
 
-    switch (type) {
-      case "begin": {
-        q.push("begin");
-        ir.push("[");
-        break;
-      }
-      case "end": {
-        const last = q.pop();
-        if (last !== "begin") {
-          console.log("error");
-        }
-        ir.push("]");
-        break;
-      }
-      case "parbegin": {
-        q.push("parbegin");
-        ir.push("(");
-        break;
-      }
-      case "parend": {
-        const last = q.pop();
-        if (last !== "parbegin") {
-          console.log("error");
-        }
-        ir.push(")");
-        break;
-      }
-      case "call": {
-        ir.push(tree.rootNode.child(i).child(0).text);
-        break;
-      }
-     }
-  }
+		switch (type) {
+			case "begin": {
+				q.push("begin");
+				ir.push("[");
+				break;
+			}
+			case "end": {
+				const last = q.pop();
+				if (last !== "begin") {
+					console.log("error");
+				}
+				ir.push("]");
+				break;
+			}
+			case "parbegin": {
+				q.push("parbegin");
+				ir.push("(");
+				break;
+			}
+			case "parend": {
+				const last = q.pop();
+				if (last !== "parbegin") {
+					console.log("error");
+				}
+				ir.push(")");
+				break;
+			}
+			case "call": {
+				ir.push(tree.rootNode.child(i).child(0).text);
+				break;
+			}
+		}
+	}
 
-  let mode = [];
-  let ids = [];
-  let blocks = new Map();
+	const stack = [];
+	let current = [];
+	for (let i = 0; i < ir.length; i++) {
+		if (ir[i] === "[") {
+			const node = { type: "seq", child: current };
+			stack.push(node);
+			current = [];
+		} else if (ir[i] === "(") {
+			const node = { type: "par", child: current };
+			stack.push(node);
+			current = [];
+		} else if (ir[i] === "]" || ir[i] === ")") {
+			const last = stack.pop();
+			const node = { type: last.type, child: current };
+			current = last.child;
+			current.push(node);
+		} else {
+			current.push({ type: "call", label: ir[i] });
+		}
+	}
 
-  for (let i = 0; i < ir.length; i++) {
-    if (ir[i] === '[') {
-      mode.push("seq");
-      ids.push(crypto.randomUUID());
+	console.log("digraph {");
+	interpret(current[0], undefined, undefined);
+	console.log("}");
 
-      if (blocks.has(ids[ids.length - 1])) {
-        blocks.get(ids[ids.length - 1]).push({ seq: ids[ids.length - 1] });
-      }
-    } else if (ir[i] === ']') {
-      mode.pop();
-      ids.pop();
-    } else if (ir[i] === '(') {
-      mode.push("par");
-      ids.push(crypto.randomUUID());
+	const cy = cytoscape({
+		container: treeContainer,
+		elements: [
+			{ data: { id: "A" } },
+			{ data: { id: "B" } },
+			{ data: { id: "C" } },
+			{ data: { id: "D" } },
+			{ data: { id: "E" } },
+			{ data: { id: "F" } },
 
-      if (blocks.has(ids[ids.length - 1])) {
-        blocks.get(ids[ids.length - 1]).push({ par: ids[ids.length - 1] });
-      }
-    } else if (ir[i][0] === ')') {
-      mode.pop();
-      ids.pop();
-    } else {
-      if (!blocks.has(ids[ids.length - 1])) {
-        blocks.set(ids[ids.length - 1], []);
-      }
-
-      blocks.get(ids[ids.length - 1]).push({ label: ir[i] });
-    }
-  }
-
-  /*
-  let graph = "digraph { ";
-
-  let last;
-
-  for (let i = 0; i < sttmts.length; i++) {
-     if (sttmts[i].next) {
-      for (let j = 0; j < sttmts[i].next.length; j++) {
-        graph = `${graph} "${sttmts[i].label}" -> "${sttmts[i].next[j].label}";`);
-      }
-
-    if (last) {
-      if (last.next) {
-        for (let j = 0; j < last.next.length; j++) {
-          graph = `${graph} "${last.next[j].label}" -> "${sttmts[i].label}";`;
-        }
-      } else {
-        graph = `${graph} "${last.label}" -> "${sttmts[i].label}";`;
-      }
-    }
-
-    last = sttmts[i];
-  }
-  graph = `${graph} }`;
-  console.log(graph);
-  */
+			{ data: { source: "A", target: "B" } },
+			{ data: { source: "A", target: "D" } },
+			{ data: { source: "B", target: "C" } },
+			{ data: { source: "D", target: "E" } },
+			{ data: { source: "C", target: "F" } },
+			{ data: { source: "E", target: "F" } },
+		],
+		layout: {
+			name: "dagre",
+		},
+		style: [
+			{
+				selector: "node",
+				css: {
+					label: "data(id)",
+				},
+			},
+		],
+	});
 })();
